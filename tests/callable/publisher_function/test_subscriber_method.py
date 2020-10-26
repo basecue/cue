ee11932f3a7395d89ef2d1bb052b7bd06b463b10
Jan Češpivo <jan.cespivo@gmail.com>
2020-10-26 21:22:49 +0100

    Fixed basic tests

--- tests/callable/publisher_function/test_subscriber_method.py
+++ tests/callable/publisher_function/test_subscriber_method.py
@@ -1,38 +1,98 @@
+from types import SimpleNamespace
+
+import pytest
+
 from cue import publisher, subscribe
 
 
-@publisher
-def event(arg, kwarg: bool = True):
-    pass
-
-
-@publisher
-def event_2(arg, arg_2, kwarg: bool = True):
-    pass
-
-
-class Subscriber:
-    @subscribe.before(event)
-    def on_event(self, arg, *, kwarg):
+@pytest.fixture
+def setup():
+    @publisher
+    def event(text: str, flag: bool = True):
         pass
 
-    @subscribe.after(event)
-    def on_event(self, arg, *, kwarg):
+    @publisher
+    def event_2(text: str, number: int, flag: bool = True):
         pass
 
-    @subscribe.before(event_2)
-    def on_event_2(self, arg, arg_2, *, kwarg):
-        pass
+    publishers = SimpleNamespace(event=event, event_2=event_2)
 
-    @subscribe.after(event_2)
-    def on_event_2(self, arg, arg_2, *, kwarg):
-        pass
+    class Subscriber:
+        subscribers = SimpleNamespace(
+            on_event_before=[],
+            on_event_after=[],
+            on_event_2_before=[],
+            on_event_2_after=[],
+            on_both_events=[],
+        )
 
-    @subscribe.before(event)
-    @subscribe.before(event_2)
-    def on_both_events(self, *args, **kwargs):
-        pass
+        @subscribe.before(event)
+        def on_event_before(self, text: str, flag: bool = True):
+            self.subscribers.on_event_before.append((self, text, flag))
+
+        @subscribe.after(event)
+        def on_event_after(self, text: str, flag: bool = True):
+            self.subscribers.on_event_after.append((self, text, flag))
+
+        @subscribe.before(event_2)
+        def on_event_2_before(self, text: str, number: int, flag: bool = True):
+            self.subscribers.on_event_2_before.append((self, text, number, flag))
+
+        @subscribe.after(event_2)
+        def on_event_2_after(self, text: str, number: int, flag: bool = True):
+            self.subscribers.on_event_2_after.append((self, text, number, flag))
+
+        @subscribe.before(event)
+        @subscribe.before(event_2)
+        def on_both_events(self, *args, **kwargs):
+            self.subscribers.on_both_events.append((self, args, kwargs))
+
+    return publishers, Subscriber
 
 
-subscriber = Subscriber()
-subscriber_2 = Subscriber()
+def test_event(setup):
+    publishers, Subscriber = setup
+    subscriber = Subscriber()
+    subscriber_2 = Subscriber()
+
+    publishers.event('text', flag=False)
+    publishers.event_2('text', 42, flag=False)
+
+    assert Subscriber.subscribers.on_event_before == [
+        (subscriber, 'text', False),
+        (subscriber_2, 'text', False),
+    ]
+    assert Subscriber.subscribers.on_event_after == [
+        (subscriber, 'text', False),
+        (subscriber_2, 'text', False),
+    ]
+    assert Subscriber.subscribers.on_event_2_before == []
+    assert Subscriber.subscribers.on_event_2_after == []
+    assert Subscriber.subscribers.on_both_events == [
+        ((subscriber, 'text',), {"flag": False}),
+        ((subscriber_2, 'text',), {"flag": False}),
+    ]
+
+
+def test_event_2(setup):
+    publishers, Subscriber = setup
+    subscriber = Subscriber()
+    subscriber_2 = Subscriber()
+
+    publishers.event('text', flag=False)
+    publishers.event_2('text', 42, flag=False)
+
+    assert Subscriber.subscribers.on_event_before == []
+    assert Subscriber.subscribers.on_event_after == []
+    assert Subscriber.subscribers.on_event_2_before == [
+        (subscriber, 'text', False),
+        (subscriber_2, 'text', False),
+    ]
+    assert Subscriber.subscribers.on_event_2_after == [
+        (subscriber, 'text', False),
+        (subscriber_2, 'text', False),
+    ]
+    assert Subscriber.subscribers.on_both_events == [
+        ((subscriber, 'text',), {"flag": False}),
+        ((subscriber_2, 'text',), {"flag": False}),
+    ]
--- tests/dataclasses/test_basics.py
+++ tests/dataclasses/test_basics.py
@@ -1,43 +1,109 @@
 import dataclasses
+from types import SimpleNamespace
 from typing import Union
+
+import pytest
 
 from cue import publisher, subscribe
 
 
-@publisher
-@dataclasses.dataclass
-class Klass:
-    text: str
-    flag: bool
+@pytest.fixture
+def setup():
+    @publisher
+    @dataclasses.dataclass
+    class Klass:
+        text: str
+        number: int
+
+    subscribers = SimpleNamespace(
+        on_change_before_text=[],
+        on_change_after_text=[],
+        on_change_before_flag=[],
+        on_change_after_flag=[],
+        on_change_before=[],
+        on_change_after=[]
+    )
+
+    @subscribe.before(Klass.text)
+    def on_change_before_text(instance: Klass, number: int):
+        subscribers.on_change_before_text.append((instance, number))
+
+    @subscribe.after(Klass.text)
+    def on_change_after_text(instance: Klass, number: int):
+        subscribers.on_change_after_text.append((instance, number))
+
+    @subscribe.before(Klass.number)
+    def on_change_before_flag(instance: Klass, number: int):
+        subscribers.on_change_before_flag.append((instance, number))
+
+    @subscribe.after(Klass.number)
+    def on_change_after_flag(instance: Klass, number: int):
+        subscribers.on_change_after_flag.append((instance, number))
+
+    @subscribe.before(Klass.text)
+    @subscribe.before(Klass.number)
+    def on_change_before(instance: Klass, value: Union[str, int]):
+        subscribers.on_change_before.append((instance, value))
+
+    @subscribe.after(Klass.text)
+    @subscribe.after(Klass.number)
+    def on_change_after(instance: Klass, value: Union[str, int]):
+        subscribers.on_change_after.append((instance, value))
+
+    return Klass, subscribers
 
 
-@subscribe.before(Klass.text)
-def on_change_before_text(instance: Klass, flag: bool):
-    pass
+def test_event(setup):
+    Klass, subscribers = setup
+    instance = Klass(text="init", number=10)
+    instance_2 = Klass(text="init_2", number=20)
 
+    instance.text = "text"
+    instance_2.text = "text_2"
+    instance.number = 30
+    instance_2.number = 40
 
-@subscribe.after(Klass.text)
-def on_change_after_text(instance: Klass, flag: bool):
-    pass
-
-
-@subscribe.before(Klass.flag)
-def on_change_before_flag(instance: Klass, flag: bool):
-    pass
-
-
-@subscribe.after(Klass.flag)
-def on_change_after_flag(instance: Klass, flag: bool):
-    pass
-
-
-@subscribe.before(Klass.text)
-@subscribe.before(Klass.flag)
-def on_change_before(instance: Klass, value: Union[str, bool]):
-    pass
-
-
-@subscribe.after(Klass.text)
-@subscribe.after(Klass.flag)
-def on_change_after(instance: Klass, value: Union[str, bool]):
-    pass
+    assert subscribers.on_change_before_text == [
+        (instance, 'init'),
+        (instance_2, 'init_2'),
+        (instance, 'text'),
+        (instance_2, 'text_2'),
+    ]
+    assert subscribers.on_change_after_text == [
+        (instance, 'init'),
+        (instance_2, 'init_2'),
+        (instance, 'text'),
+        (instance_2, 'text_2'),
+    ]
+    assert subscribers.on_change_before_flag == [
+        (instance, 10),
+        (instance_2, 20),
+        (instance, 30),
+        (instance_2, 40),
+    ]
+    assert subscribers.on_change_after_flag == [
+        (instance, 10),
+        (instance_2, 20),
+        (instance, 30),
+        (instance_2, 40),
+    ]
+    assert subscribers.on_change_before == [
+        (instance, 'init'),
+        (instance, 10),
+        (instance_2, 'init_2'),
+        (instance_2, 20),
+        (instance, 'text'),
+        (instance_2, 'text_2'),
+        (instance, 30),
+        (instance_2, 40),
+    ]
+    assert subscribers.on_change_after == [
+        (instance, 'init'),
+        (instance, 10),
+        (instance_2, 'init_2'),
+        (instance_2, 20),
+        (instance, 'text'),
+        (instance_2, 'text_2'),
+        (instance, 30),
+        (instance_2, 40),
+    ]
